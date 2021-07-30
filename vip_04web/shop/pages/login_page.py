cb5a1e9f77bb2263a8442ecd780966e93e30af4c
jijianfeng <1065109432@qq.com>
2021-07-30 16:09:04 +0800

    666

---
+++ vip_04web/shop/pages/login_page.py
@@ -0,0 +1,40 @@
+# coding = utf-8
+from selenium.webdriver.common.by import By
+
+from vip_04web.shop.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    # 定义元素定位方式和属性值
+    index_login_btn = (By.XPATH, '/html/body/div[2]/div/ul[1]/div/div/a[1]')
+    user_input = (By.CSS_SELECTOR, '[placeholder="请输入用户名/手机/邮箱"]')
+    pwd_input = (By.CSS_SELECTOR, '[placeholder="请输入登录密码"]')
+    login_btn = (By.XPATH, '/html/body/div[4]/div/div[2]/div[2]/div/div/div[1]/form/div[3]/button')
+    quit_btn = (By.LINK_TEXT,'退出')
+    error_msg = (By.XPATH,'/html/body/div[8]/div/p')
+    # 元素操作
+    def click_index_login_btn(self):
+        self.ele_click(LoginPage.index_login_btn)
+
+    def input_user_input(self, user):
+        self.input_text(LoginPage.user_input, user)
+
+    def input_pwd_input(self, pwd):
+        self.input_text(LoginPage.pwd_input, pwd)
+
+    def click_login_btn(self):
+        self.ele_click(LoginPage.login_btn)
+
+    def find_quit_bin(self):
+        return self.ele_wait(LoginPage.quit_btn)
+
+    def find_error_msg(self):
+        ele = self.ele_wait(LoginPage.error_msg)
+        return ele.text
+
+    # 业务绑定——注册流程
+    def login(self, user, pwd):
+        self.click_index_login_btn()
+        self.input_user_input(user)
+        self.input_pwd_input(pwd)
+        self.click_login_btn()
---
+++ vip_04web/shop/pages/search_page.py
@@ -0,0 +1,31 @@
+# coding = utf-8
+from selenium.webdriver.common.by import By
+
+from vip_04web.shop.base_page import BasePage
+
+
+class SearchPage(BasePage):
+    # 定义元素定位方式和属性值
+    search_input = (By.ID, 'search-input')
+    search_btn = (By.XPATH, '//*[@id="ai-topsearch"]')
+    search_goods = (By.CLASS_NAME, 'goods-images')
+
+    # 元素操作
+    def input_search_input(self, search_name):
+        self.input_text(SearchPage.search_input, search_name)
+
+    def click_search_btn(self):
+        self.ele_click(SearchPage.search_btn)
+
+    def find_search_goods(self):
+        ele_list = self.eles_list_wait(SearchPage.search_goods)
+        ele_list_goods_name = []
+        for i in ele_list:
+            ele_goods_name = i.get_attribute('alt')
+            ele_list_goods_name.append(ele_goods_name)
+        return ele_list_goods_name
+
+    # 业务绑定——搜索流程
+    def search(self, search_name):
+        self.input_search_input(search_name)
+        self.click_search_btn()
